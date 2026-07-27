be6a41eabc6c25b64c74c1d9af7cad7c8a6a6a7c
Muralidhar Vijayagiri <murali.github07@gmail.com>
2026-07-28 02:12:57 +0530

    Intialized the springboot

---
+++ src/main/java/com/murali/SpringBoot_Beginner/SpringBootBeginnerApplication.java
@@ -0,0 +1,13 @@
+package com.murali.SpringBoot_Beginner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBeginnerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBeginnerApplication.class, args);
+	}
+
+}
